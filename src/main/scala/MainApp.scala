a7aec4e4925a1fb11caac298029c8ac2089dc576
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2020-05-10 03:07:58 +0700

    Remove MainApp - move to Unit Test

--- src/main/scala/MainApp.scala
+++
@@ -1,9 +0,0 @@
-import com.app.protobuf.change.ChangeMessage
-
-object MainApp extends App {
-  val changeMessage = ChangeMessage(
-    id = 1,
-    changeFrom = "2020-01-01",
-    changeTo = "2020-02-01",
-  )
-}
